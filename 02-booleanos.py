5088601ad4afef8316f5179b29baf0384424bd23
Alvaro-Torres <alvarobastian.torres@alumnos.ulagos.cl>
2025-06-23 18:35:28 -0400

    "Subiendo archivo de booleanos al repositorio"

---
+++ 02-booleanos.py
@@ -0,0 +1,9 @@
+interruptor = True
+ampolleta = False
+
+# Función type permite saber el tipo de dato que se utiliza
+print(type(interruptor))
+
+# Comparativa de valores lógicos
+print(1<10)
+print(100<=20)
